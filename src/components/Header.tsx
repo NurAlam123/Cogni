2579812f578e04bb1158e65748fc9b9cc45cc243
Nur Alam <nuralam.rsc@gmail.com>
2024-10-24 21:44:51 +0600

    fix: fix blur effect in header section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,9 @@ import Button from "./ui/Button";
 const Header = () => {
   return (
     <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
+      <div className="absolute inset-0 backdrop-blur md:backdrop-blur-none -z-10" />
       <div className="container">
-        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto backdrop-blur">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto md:backdrop-blur">
           <div>
             <div className="border h-10 w-10 rounded-lg inline-flex justify-center border-white/15 items-center">
               <LogoIcon className="h-8 w-8" />
